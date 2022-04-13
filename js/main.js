f8ee935255abf961092da96fa289fb0d3d14822f
Fitri Febriani <fitrifebriani99@gmail.com>
2022-04-13 10:09:13 +0700

    [ADD] LP Full  Page

---
+++ js/main.js
@@ -0,0 +1,149 @@
+window.addEventListener("load", () => {
+  /* ----------------------------
+      page loader
+  ------------------------------- */
+  document.querySelector(".js-page-loader").classList.add("fade-out");
+  setTimeout(() => {
+    document.querySelector(".js-page-loader").style.display = "none";
+  }, 600);
+});
+
+/* ----------------------------
+    course preview video
+------------------------------- */
+function coursePreviewVideo() {
+  const coursePreviewVideo = document.querySelector(".js-course-preview-modal");
+  if (coursePreviewVideo) {
+    // if element exist
+    coursePreviewVideo.addEventListener("shown.bs.modal", function () {
+      this.querySelector(".js-course-preview-video").play();
+      this.querySelector(".js-course-preview-video").currentTime = 0;
+    });
+
+    coursePreviewVideo.addEventListener("hide.bs.modal", function () {
+      this.querySelector(".js-course-preview-video").pause();
+    });
+  }
+}
+coursePreviewVideo();
+
+/* ----------------------------
+    header menu
+------------------------------- */
+function headerMenu() {
+  const menu = document.querySelector(".js-header-menu"),
+    backdrop = document.querySelector(".js-header-backdrop"),
+    menuCollapseBreakpoint = 991;
+
+  function toggleMenu() {
+    menu.classList.toggle("open");
+    backdrop.classList.toggle("active");
+    document.body.classList.toggle("overflow-hidden");
+  }
+
+  document.querySelectorAll(".js-header-menu-toggler").forEach((item) => {
+    item.addEventListener("click", toggleMenu);
+  });
+
+  // close the menu by click outside of it
+  backdrop.addEventListener("click", toggleMenu);
+
+  function collapse() {
+    menu.querySelector(".active .js-sub-menu").removeAttribute("style");
+    menu.querySelector(".active").classList.remove("active");
+  }
+
+  menu.addEventListener("click", (event) => {
+    const { target } = event;
+
+    if (target.classList.contains("js-toggle-sub-menu") && window.innerWidth <= menuCollapseBreakpoint) {
+      // prevent default anchor click behavior
+      event.preventDefault();
+
+      // if menu already expanded, collapse it and exit
+      if (target.parentElement.classList.contains("active")) {
+        collapse();
+        return;
+      }
+
+      // if not already expanded, run code below
+
+      // collapse the other expanded menu item if exists
+      if (menu.querySelector(".active")) {
+        collapse();
+      }
+
+      // expand new menu-item
+      target.parentElement.classList.add("active");
+      target.nextElementSibling.style.maxHeight = target.nextElementSibling.scrollHeight + "px";
+    }
+  });
+
+  // when resizing window
+  window.addEventListener("resize", function () {
+    if (this.innerWidth > menuCollapseBreakpoint && menu.classList.contains("open")) {
+      toggleMenu();
+    }
+    if (this.innerWidth > menuCollapseBreakpoint && menu.querySelector(".active")) {
+      collapse();
+    }
+  });
+}
+headerMenu();
+
+/* ----------------------------
+    theme colors
+------------------------------- */
+function themeColors() {
+  const colorStyle = document.querySelector(".js-color-style"),
+    themeColorContainer = document.querySelector(".js-theme-colors");
+
+  themeColorContainer.addEventListener("click", ({ target }) => {
+    if (target.classList.contains("js-theme-color-item")) {
+      localStorage.setItem("color", target.getAttribute("data-js-theme-color"));
+      setColor();
+    }
+  });
+  function setColor() {
+    let path = colorStyle.getAttribute("href").split("/");
+    path = path.slice(0, path.length - 1);
+    colorStyle.setAttribute("href", path.join("/") + "/" + localStorage.getItem("color") + ".css");
+
+    if (document.querySelector(".js-theme-color-item.active")) {
+      document.querySelector(".js-theme-color-item.active").classList.remove("active");
+    }
+    document.querySelector("[data-js-theme-color=" + localStorage.getItem("color") + "]").classList.add("active");
+  }
+  if (localStorage.getItem("color") !== null) {
+    setColor();
+  } else {
+    const defaultColor = colorStyle.getAttribute("href").split("/").pop().split(".").shift();
+    document.querySelector("[data-js-theme-color=" + defaultColor + "]").classList.add("active");
+  }
+}
+themeColors();
+
+/* ----------------------------
+    gallery pop up
+------------------------------- */
+function galleryModal() {
+  const galleryPreview = document.querySelector(".js-gallery");
+  if (coursePreviewVideo) {
+    // if element exist
+    coursePreviewVideo.addEventListener("shown.bs.modal", function () {
+      this.querySelector(".js-course-preview-video").play();
+      this.querySelector(".js-course-preview-video").currentTime = 0;
+    });
+
+    coursePreviewVideo.addEventListener("hide.bs.modal", function () {
+      this.querySelector(".js-course-preview-video").pause();
+    });
+  }
+  document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("gallery-item")) {
+      const src = e.target.getAttribute("src");
+      console.log(src);
+    }
+  });
+}
+galleryModal();
