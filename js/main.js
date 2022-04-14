28c9093b7b5dbadecb039b0415d588e519363332
Fitri Febriani <fitrifebriani99@gmail.com>
2022-04-14 10:40:29 +0700

    [ADD] Revisi Mobile Ver

--- js/main.js
+++ js/main.js
@@ -9,25 +9,6 @@ window.addEventListener("load", () => {
 });
 
 /* ----------------------------
-    course preview video
-------------------------------- */
-function coursePreviewVideo() {
-  const coursePreviewVideo = document.querySelector(".js-course-preview-modal");
-  if (coursePreviewVideo) {
-    // if element exist
-    coursePreviewVideo.addEventListener("shown.bs.modal", function () {
-      this.querySelector(".js-course-preview-video").play();
-      this.querySelector(".js-course-preview-video").currentTime = 0;
-    });
-
-    coursePreviewVideo.addEventListener("hide.bs.modal", function () {
-      this.querySelector(".js-course-preview-video").pause();
-    });
-  }
-}
-coursePreviewVideo();
-
-/* ----------------------------
     header menu
 ------------------------------- */
 function headerMenu() {
